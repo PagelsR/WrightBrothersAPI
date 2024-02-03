f910bd1f17bb88ededb062293e44030e1e11e8ad
Randy Pagels <pagelsr@live.com>
2024-02-03 14:49:07 -0500

    Add FlightLogSignature and AerobaticSequence models*** ***Add Microsoft.Extensions.Diagnostics.HealthChecks package*** ***Add health check services and endpoint mapping

---
+++ WrightBrothersApi/WrightBrothersApi/Models/FlightLogSignature.cs
@@ -0,0 +1,8 @@
+public class FlightLog
+{
+    public string Date { get; set; }
+    public string Departure { get; set; }
+    public string Arrival { get; set; }
+    public string FlightNumber { get; set; }
+}
+
